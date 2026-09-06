65442bc4d3ae54b8749a9ded1163047b65964013
Hardik Agarwal <agarwalhardik549@gmail.com>
2026-09-06 22:47:41 +0530

    Time: 17 ms (53.15%), Space: 26.1 MB (52.54%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for (string& s : strs) {
+            string key(26, '0');
+            for (char c : s) {
+                key[c - 'a']++;
+            }
+            mp[key].push_back(s);
+        }
+        vector<vector<string>> ans;
+        for (auto& [key, group] : mp) {
+            ans.push_back(move(group));
+        }
+        return ans;
+    }
+};
